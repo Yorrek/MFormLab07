6a8bf789e3380b38c59cef12d59698fc25a55881
CranCraft <Felix@dhcp-vl203-11-231.hs-bremen.de>
2016-12-19 12:02:28 +0100

    Fehlerbehebung

--- CubicBezier.cpp
+++ CubicBezier.cpp
@@ -19,58 +19,6 @@ CubicBezier::CubicBezier() {
 CubicBezier::~CubicBezier() {
 }
 
-/*
-void CubicBezier::Subdivide(float t, CubicBezier *segment1, CubicBezier *segment2) {
-    *segment1->setPoint(1,this->getPoint(1)); // erster punkt check
-    *segment2->setPoint(1,this->getPoint(4));
-    Point pTmp;
-    //Berechnung der 1. Generation
-    for (int i = 0; i < 3; i++){
-        Point p1 = this->getPoint(i+1);
-        p1.setX(p1.getX()*(1-t));
-        p1.setY(p1.getY()*(1-t));
-        Point p2 = this->getPoint(i+2);
-        p2.setX(p2.getX()*t);
-        p2.setY(p2.getY()*t);
-        if(i==0){
-            *segment1->setPoint(2,p1+p2); // zweiter punkt check
-        }
-        else if (i==2){
-            *segment2->setPoint(2,p1+p2);
-        }
-        else {
-            pTmp = new Point(p1 + p2);
-        }
-    }
-    Point pTmp2 = pTmp;
-    //Berechnung der 2. generation
-    Point p1 = *segment1->getPoint(2);
-
-    p1.setX(p1.getX()*(1-t));
-    p1.setY(p1.getY()*(1-t));
-    pTmp.setX(pTmp.getX()*t);
-    pTmp.setY(pTmp.getY()*t);
-    *segment1->setPoint(3,p1+pTmp);
-
-    pTmp2.setX(pTmp2.getX()*(1-t));
-    pTmp2.setY(pTmp2.getY()*(1-t));
-    p1 = *segment2->getPoint(2);
-    p1.setX(p1.getX()*t);
-    p1.setY(p1.getY()*t);
-    *segment2->setPoint(3,p1+pTmp2);
-
-    p1 = *segment1->getPoint(3);
-    Point p2 = *segment2->getPoint(3);
-    p1.setX(p1.getX()*(1-t));
-    p1.setY(p1.getY()*(1-t));
-    p2.setX(p2.getX()*t);
-    p2.setY(p2.getY()*t);
-    *segment1->setPoint(4,p1+p2);
-    *segment2->setPoint(4,p1+p2);
-}
-*/
-
-
 void CubicBezier::Subdivide(float t, CubicBezier *segment1, CubicBezier *segment2) {
 
     Point pTmp; //Temporärer speicher für einen Punkt
@@ -136,23 +84,18 @@ void CubicBezier::Subdivide(float t, CubicBezier *segment1, CubicBezier *segment
 
 void CubicBezier::multipliyMatrix(float t, CubicBezier *aMatrix, CubicBezier *bMatrix) {
 
-    float product1[4][4] =   {{1,                         0,                         0,                   0},
+    float product1[4][4] = {{1,                           0,                         0,                   0},
                             {(1 - t),                     t,                         0,                   0},
                             {(1 - t) * (1 - t),           2 * (1 - t) * t,           t * t,               0},
                             {(1 - t) * (1 - t) * (1 - t), 3 * (1 - t) * (1 - t) * t, 3 * (1 - t) * t * t, t * t * t}};
 
-    float product2[4][4] =  {{0,          0,              0,                             1},
-                            {0,           0,              1,                            (1-t)},
-                            {0,           t * t,          2 * (1 -t) * t,               (1 - t) * (1 -t)},
-                            {t * t * t,   3 * (1 -t) * t * t,  3 * (1 -t) * (1 -t) * t, (1 - t) * (1 - t) * (1 - t)}};
+    float product2[4][4] = {{0,         0,                   0,                         1},
+                            {0,         0,                   t,                         (1 - t)},
+                            {0,         t * t,               2 * (1 - t) * t,           (1 - t) * (1 - t)},
+                            {t * t * t, 3 * (1 - t) * t * t, 3 * (1 - t) * (1 - t) * t, (1 - t) * (1 - t) * (1 - t)}};
 
-
-
-        //int rows = sizeof product / sizeof product[0];
-        //int cols = sizeof product[0] / sizeof(int);
-
-    for (int i = 0; i < 4; i++){
-        for ( int j = 0; j < 4; j++){
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             subDivMatrix1[i].m_x += product1[i][j] * aMatrix->points[j].m_x;
             subDivMatrix1[i].m_y += product1[i][j] * aMatrix->points[j].m_y;
             subDivMatrix2[i].m_x += product2[i][j] * bMatrix->points[j].m_x;
@@ -161,35 +104,14 @@ void CubicBezier::multipliyMatrix(float t, CubicBezier *aMatrix, CubicBezier *bM
     }
 
     cout << "subDivMatrix für die rechte Hälfte" << endl;
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         cout << "Stelle " << i << " " << "x: " << subDivMatrix1[i].m_x << "Y: " << subDivMatrix1[i].m_y << endl;
     }
 
     cout << "subDivMatrix für die linke Hälfte" << endl;
-    for (int i = 0; i < 4; i++){
-        cout << "Stelle " << i << " " << "x: " <<  subDivMatrix2[i].m_x << "Y: " << subDivMatrix1[i].m_y << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << "Stelle " << i << " " << "x: " << subDivMatrix2[i].m_x << "Y: " << subDivMatrix1[i].m_y << endl;
     }
-    /* for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-            subDivMatrix[i][j] = Point(0,0);
-            for (int k = 0; k < 4; k++){
-                subDivMatrix[i][j].m_x = subDivMatrix[i][j].m_x + (aMatrix[i][k]->m_x * bMatrix[k][j]->m_x);
-                subDivMatrix[i][j].m_y = subDivMatrix[i][j].m_y + (aMatrix[i][k]->m_y * bMatrix[k][j]->m_y);
-            }
-        }
-    }
-    */
-
-    /*
-        for (int i = 0; i < 4; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                subDivMatrix[i][j] = Point (0,0);
-                for (int k = 0; k < 4; ++k) {
-                    subDivMatrix[i][j] = subDivMatrix[i][j] + (aMatrix[i][k] * bMatrix[k][j]);
-                }
-            }
-        }
-    }*/
 
 }
 
